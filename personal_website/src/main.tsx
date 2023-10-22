a4f611f108b40e39a83c6d32bd7c9c11b10bc263
icarus2176 <ashe.kemuri@gmail.com>
2023-10-22 13:41:54 -0500

    create pages and routes

--- personal_website/src/main.tsx
+++ personal_website/src/main.tsx
@@ -1,16 +1,39 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './pages/App.tsx';
+import Home from './pages/Home .tsx';
+import About from '.pages/About.tsx';
+import Project from './pages/Project.tsx';
+import Contact from './pages/Contact.tsx'
+import ErrorPage from './pages/Error.tsx';
 import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
-import './index.css'
+import './index.css';
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world!</div>,
+    element: <Home />,
+  },
+
+ { 
+    path: "/about",
+    element: <About />,
+    errorElement: <ErrorPage />
+  },
+
+  {
+    path: "/projects",
+    element: <Project />,
+    errorElement: <ErrorPage />
+  },
+
+  {
+    path: "/contact",
+    element: <Contact />,
+    errorElement: <ErrorPage />
   },
 ]);
 
