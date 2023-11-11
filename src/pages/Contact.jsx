bf849868b471aef94ffcf2aa0fa32bf979524214
icarus2176 <ashe.kemuri@gmail.com>
2023-11-11 14:31:15 -0600

    change styling

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -6,7 +6,7 @@ import "./Contact.css"
 export default function Contact() {
   return(
     <div className="main contact">
-      <h1>Where to Find Me</h1>
+      <h1>Contact Me</h1>
       <div className="row">
         <img className="logo" src={githubLogo} alt="Github Logo"></img>
         <a href="https://github.com/icarus2176">My Github Account</a>
