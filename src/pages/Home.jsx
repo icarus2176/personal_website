1f1074bb8e0672a43b6ed9913577e61bcf9a3975
icarus2176 <ashe.kemuri@gmail.com>
2024-03-12 01:44:42 -0500

    refine mobile design

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@ import "./Home.css"
 
 export default function Home() {
   return(
-    <div className="home main">
+    <div className="home">
       <title>Ashe Kemuri</title>
       <h3 className="top">Welcome, I'm</h3>
       <h1>ASHE KEMURI</h1>
